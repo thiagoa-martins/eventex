0d021c3c56a1a82191c5de656fbdfb9428fba453
Thiago <thiagoamartins0@gmail.com>
2025-07-07 23:34:01 -0300

    Update host

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['eventex-2mcv.onrender.com']
+ALLOWED_HOSTS = ['eventex-hfwz.onrender.com']
 
 
 # Application definition
